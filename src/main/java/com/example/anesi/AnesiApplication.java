bbd7e11a5f605a425f0fd5dd5dfebdc8f0f385cc
Jeongseonil <qazs632145@gmail.com>
2023-08-07 17:20:38 +0900

    start

---
+++ src/main/java/com/example/anesi/AnesiApplication.java
@@ -0,0 +1,13 @@
+package com.example.anesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anesi/AnesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
